c5b5e293e8093b2141936970c86351ee5d53b7aa
Colin Wong <cwongmath@gmail.com>
2022-04-04 23:57:21 -0500

    Add separate blue, tie dye

--- src/main/java/frc/team5431/shuffle/TitanShufflePlugin.java
+++ src/main/java/frc/team5431/shuffle/TitanShufflePlugin.java
@@ -8,15 +8,17 @@ import edu.wpi.first.shuffleboard.api.theme.Theme;
 
 import java.util.List;
 
-@Description(group = "frc.team5431.shuffle", name = "TitanShufflePlugin", version = "0.0.3", summary = "5431's Shuffleboard Plugin")
+@Description(group = "frc.team5431.shuffle", name = "TitanShufflePlugin", version = "0.0.5", summary = "5431's Shuffleboard Plugin")
 public class TitanShufflePlugin extends Plugin {
 
+  private static final Theme themeTitanLight = new Theme(TitanShufflePlugin.class, "Titan Light", "/frc/team5431/shuffle/titanlight.css");
     private static final Theme themeTitanDark = new Theme(TitanShufflePlugin.class, "Titan Dark", "/frc/team5431/shuffle/titandark.css");
-    private static final Theme themeTitanLight = new Theme(TitanShufflePlugin.class, "Titan Light", "/frc/team5431/shuffle/titanlight.css");
+    private static final Theme themeTitanBlue = new Theme(TitanShufflePlugin.class, "Titan Blue", "/frc/team5431/shuffle/titanblue.css");
+    private static final Theme themeTitanTieDye = new Theme(TitanShufflePlugin.class, "Titan Tie Dye", "/frc/team5431/shuffle/titantiedye.css");
 
     @Override
     public List<Theme> getThemes() {
-      return ImmutableList.of(themeTitanDark, themeTitanLight);
+      return ImmutableList.of(themeTitanLight, themeTitanDark, themeTitanBlue, themeTitanTieDye);
     }
 
 }
